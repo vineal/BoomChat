daf98b7c32df754345d5ce2f00fdb025e15ae6b6
Vineal Viji Varghese <vinealviji@gmail.com>
2020-07-19 16:13:38 +0530

    Final Commit

---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,8 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Vineal Viji Varghese on 17/07/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
---
+++ Flash Chat iOS13/Models/Messages.swift
@@ -0,0 +1,14 @@
+//
+//  Messages.swift
+//  Flash Chat iOS13
+//
+//  Created by Vineal Viji Varghese on 18/07/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender : String
+    let body : String
+}
